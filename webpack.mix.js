aad86825e5a688c9f3d0431defb1ca80c5e66fe0
Carlos Amores <carlosm.amocad@educa.jcyl.es>
2020-12-09 11:41:14 +0100

    Complete SCSS update.

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,15 @@ mix.webpackConfig(
 );
 
 // Stylesheets.
+mix.sass('Divs/style.scss', 'Divs/style.css');
+mix.sass('Ej-enlaces/style.scss', 'Ej-enlaces/style.css');
+mix.sass('Ej-examen1/style.scss', 'Ej-examen1/style.css');
+mix.sass('Ej-imagenes/style.scss', 'Ej-imagenes/style.css');
 mix.sass('Ej-listas/style.scss', 'Ej-listas/style.css');
+mix.sass('Ej-listas2/style.scss', 'Ej-listas2/style.css');
+mix.sass('Ej-listas3/style.scss', 'Ej-listas3/style.css');
+mix.sass('Ej-museo/style.scss', 'Ej-museo/style.css');
+mix.sass('InternetEnEspaña/style.scss', 'InternetEnEspaña/style.css');
 
 // JavaScript.
 mix.js('bootstrap.ts', 'bootstrap.js');
